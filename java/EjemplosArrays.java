1259a602e9d125836ebd69b7e881517edb00dc5f
Ricardo Pérez <ricpelo@gmail.com>
2025-05-15 19:28:06 +0200

    Jueves 15 de mayo

--- java/EjemplosArrays.java
+++ java/EjemplosArrays.java
@@ -1,13 +1,27 @@
 public class EjemplosArrays {
     public static void main(String[] args) {
-        int[] a = new int[5];
-        int[] b = a;
+        int[] a = new int[] { 10, 20, 30, 40, 50 };
+        String[] s = new String[] { "Hola", null, "María" };
+        int suma;
 
-        a[0] = 25;
-        a[1] = 33;
+        for (int i = suma = 0; i < a.length; i++) {
+            suma += a[i];
+        }
 
-        System.out.println(a[0]);
-        System.out.println(b[0]);
-        System.out.println(a[3]);
+        System.out.println(suma);
+        imprimirOrdenInverso(a);
+    }
+
+    public static void imprimirOrdenInverso(int[] ar) {
+        for (int i = ar.length - 1; i >= 0; i--) {
+            System.out.println(ar[i]);
+        }
+    }
+
+    /* Devuelve true si el valor está dentro del array,
+     * o false en caso contrario.
+     */
+    public static boolean buscar(int[] ar, int valor) {
+        ;
     }
 }
--- java/Multiplicar.java
+++ java/Multiplicar.java
@@ -2,8 +2,18 @@ public class Multiplicar {
     public static void main(String[] args) {
         final int n = 10;
 
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(n + " x " + i + " = " + (n * i));
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 10; i++, vaciar(sb)) {
+            sb.append(n);
+            sb.append(" x ");
+            sb.append(i);
+            sb.append(" = ");
+            sb.append(n * i);
+            System.out.println(sb);
         }
+    }
+
+    private static void vaciar(StringBuilder s) {
+        s.delete(0, s.length());
     }
 }
